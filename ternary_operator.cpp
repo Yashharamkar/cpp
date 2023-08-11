56613f2179a447c1d50e62af04726b984cc190dd
Yashharamkar <87219809+Yashharamkar@users.noreply.github.com>
2023-08-11 12:09:16 +0530

    Create ternary_operator.cpp

---
+++ ternary_operator.cpp
@@ -0,0 +1,10 @@
+// ternary oerator -- it is a short form of if and else statement
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout<< "this is ternary operator \n",(4>10 ? cout<<"true\n" : cout<<"false\n");  
+	cout<< "this is ternary operator \n",(40>10 ? cout<<"true\n" : cout<<"false\n");  
+	return 0;
+}
