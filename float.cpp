9a947a0484ca133cf1df4ef6db0c35a5d7fe4d89
Yashharamkar <87219809+Yashharamkar@users.noreply.github.com>
2023-08-11 17:13:43 +0530

    Create float.cpp

---
+++ float.cpp
@@ -0,0 +1,14 @@
+//3type of precision 
+// float
+// double
+// long double
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	float st=20.2;
+	printf("the value of %f",st);
+	return 0;
+}
